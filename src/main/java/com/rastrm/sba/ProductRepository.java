25471e8bf17e727eeccdae27a9e243b1194f53fd
RastrigaMargarita <rastrm@mail.ru>
2021-06-02 01:40:23 +0300

    Initial commit

---
+++ src/main/java/com/rastrm/sba/ProductRepository.java
@@ -0,0 +1,26 @@
+package com.rastrm.sba;
+
+import com.rastrm.sba.Product;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class ProductRepository {
+    private final List<Product> productList = new ArrayList<Product>();
+
+    public void newProduct(Product product, String title) {
+        product.setTitle(title);
+        productList.add(product);
+    }
+
+    public String getAllList() {
+        return productList.toString();
+    }
+
+    public void delByID(int ID) {
+        productList.remove(ID);
+    }
+
+}
